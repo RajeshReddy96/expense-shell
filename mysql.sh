7903756c5cf9c5b4a77abd3d3b41ae66cc7531f7
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-09-01 08:35:16 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -34,16 +34,16 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>LOG_FILE
 VALIDATE $? "Installing mysql server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>LOG_FILE
 VALIDATE $? "Enabled mysql server"
 
-systemctl start mysqld
+systemctl start mysqld &>>LOG_FILE
 VALIDATE $? "started mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG_FILE
 VALIDATE $? "Setting up root password"
 
 
