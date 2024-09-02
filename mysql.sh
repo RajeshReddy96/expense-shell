f7c10d907c553fa345207f8588620474e5479f00
Rajeshreddyk <rajeshreddy.k.315@gmail.com>
2024-09-02 10:29:55 +0530

    variables

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
